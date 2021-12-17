54b1f10fa12db08debc7c1c169b951aa88624a8d
Seetharaman <pmsseetharaman7@gmail.com>
2021-12-17 17:16:00 +0530

    api calls are completed

--- config/config.js
+++ config/config.js
@@ -1,14 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const dataBase = new Sequelize("test", "root", "admin@123",{
+const dataBase = new Sequelize("test", "root", "",{
     host: 'localhost',
     dialect: 'mysql',
-    pool:{
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    logging: null
+    // pool:{
+    //     max: 5,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000
+    // }
 });
 
 module.exports = dataBase;
--- routes/sample.js
+++ routes/sample.js
@@ -13,4 +13,57 @@ router.post("/addTheDetails", async(req,res)=>{
     }
 });
 
+// get the details
+router.get("/getTheDetails", async(req,res)=>{
+    try {
+        const data = await details.findAll();
+        if(data.length > 0){
+            return res.status(200).json({'status': "Success", 'message': "Details added successfully", data});
+        }else{
+            return res.json({'status': "Success", 'message': "No data found"});
+        }
+    } catch (err) {
+        return res.status(400).json({'status': "Failed", 'message': err.message});
+    }
+});
+
+router.get('/getDataById', async(req,res)=>{
+    try {
+        const data = await details.findByPk(req.query.id);
+        if(data){
+            return res.status(200).json({'status': "Success", 'message': "Details added successfully", data});
+        }else{
+            return res.json({'status': "Success", 'message': "No data found"});
+        }
+    } catch (err) {
+        return res.status(400).json({'status': "Failed", 'message': err.message});
+    }
+});
+
+// delete the data
+router.delete("/deleteTheRecord", async(req,res)=>{
+    try {
+        const data = await details.destroy({ where: { id: req.query.id } });
+        return res.status(200).json({'status': "Success", 'message': "Details successfully deleted"});
+    } catch (err) {
+        return res.status(400).json({'status': "Failed", 'message': err.message});
+    }
+});
+
+// update the details
+router.put("/updateDetails", async(req,res)=>{
+    try {
+        var updateData = {
+            name:req.body.name,
+            technology:req.body.technology,
+            description:req.body.description,
+            mobile:req.body.mobile
+        }
+        const data = await details.update(updateData, { where: { id:req.body.id } })
+        return res.status(200).json({'status': "Success", 'message': "Details are successfully updated"});
+    } catch (error) {
+        return res.status(400).json({'status': "Failed", 'message': error.message});
+    }
+});
+
 module.exports = router;
